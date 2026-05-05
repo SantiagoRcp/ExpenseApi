3b7f034763671f086f1487bf757cb6a47ca982a1
SantiagoRc <santiagorcp.9854@gmail.com>
2026-05-04 20:04:55 -0600

    chore: add PrismaPg config adapter for prisma

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,3 +1,9 @@
 import {PrismaClient} from "@prisma/client";
+import {PrismaPg} from "@prisma/adapter-pg";
+import {env} from '../config/envConfig.js'
 
-export const prisma = new PrismaClient()
+const connectionString = env.DATABASE_URL;
+const adapter = new PrismaPg({connectionString});
+
+
+export const prisma = new PrismaClient({adapter})
