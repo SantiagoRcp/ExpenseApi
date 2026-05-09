df33f1e8ac577da55f9a0ed34cce9c2c1bfe23be
SantiagoRc <santiagorcp.9854@gmail.com>
2026-05-09 16:26:49 -0600

    refactor(middleware): make validateZod generic to support body, params, and query validation

--- src/middlewares/validateZod.ts
+++ src/middlewares/validateZod.ts
@@ -2,16 +2,18 @@ import {type ZodSchema} from "zod"
 import type {Request, Response, NextFunction} from "express";
 import {BadRequestError} from "../shared/errors/index.js";
 
-export function validateZod(zodSchema: ZodSchema) {
+type Source = 'body' | 'params' | 'query'
+
+export function validateZod(zodSchema: ZodSchema, source: Source = "body") {
     return (req: Request, _res: Response, next: NextFunction) => {
         try {
-            const data = zodSchema.safeParse(req.body);
+            const data = zodSchema.safeParse(req[source]);
 
             if (!data.success) {
                 const errors = data.error.issues.map(error => ({message: error.message, path: error.path}));
                 return next(new BadRequestError('Error validating data', {errors}));
             }
-            req.body = data.data
+            req[source] = data.data
             next();
         } catch (error) {
             next(error);
